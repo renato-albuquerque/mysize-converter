0bc0bbff8bae63c03bda273659878c2e217daba3
Renato Albuquerque <renatomalb83@gmail.com>
2023-11-10 11:04:55 -0300

    updated function btnSearch(), added features for choice bra-eur / updated html and css file

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ const messageCountryChoice = document.querySelector(".messageCountryChoice");
 const result = document.querySelector(".result");
 
 const table = document.querySelector(".table")
+
 const table_bra_usa_male_shirt = document.querySelector(".table_bra_usa_male_shirt");
 const table_bra_usa_male_pants = document.querySelector(".table_bra_usa_male_pants");
 const table_bra_usa_male_shoe = document.querySelector(".table_bra_usa_male_shoe");
@@ -21,6 +22,16 @@ const table_bra_usa_female_shoe = document.querySelector(".table_bra_usa_female_
 const table_bra_usa_children_shirt = document.querySelector(".table_bra_usa_children_shirt");
 const table_bra_usa_children_pants = document.querySelector(".table_bra_usa_children_pants");
 const table_bra_usa_children_shoe = document.querySelector(".table_bra_usa_children_shoe");
+
+const table_bra_eur_male_shirt = document.querySelector(".table_bra_eur_male_shirt");
+const table_bra_eur_male_pants = document.querySelector(".table_bra_eur_male_pants");
+const table_bra_eur_male_shoe = document.querySelector(".table_bra_eur_male_shoe");
+const table_bra_eur_female_shirt = document.querySelector(".table_bra_eur_female_shirt");
+const table_bra_eur_female_pants = document.querySelector(".table_bra_eur_female_pants");
+const table_bra_eur_female_shoe = document.querySelector(".table_bra_eur_female_shoe");
+const table_bra_eur_children_shirt = document.querySelector(".table_bra_eur_children_shirt");
+const table_bra_eur_children_pants = document.querySelector(".table_bra_eur_children_pants");
+const table_bra_eur_children_shoe = document.querySelector(".table_bra_eur_children_shoe");
 
 let valueCountryOrigin, valueCountryDestiny, valueCategory, valueClothingType
 
@@ -154,9 +165,59 @@ function btnSearch() {
         /* add for other tables: classlist.add.hide */
         table_bra_usa_children_shoe.classList.remove("hide");
 
+    /* ### bra - eur ### */
+
+    /* choice: bra - eur - male */
+    } else if((valueCountryOrigin == "bra" && valueCountryDestiny == "eur" && valueCategory == "male" && valueClothingType == "shirt") || (valueCountryOrigin == "eur" && valueCountryDestiny == "bra" && valueCategory == "male" && valueClothingType == "shirt")) {
+        result.classList.add("hide");
+        /* add for other tables: classlist.add.hide */
+        table_bra_eur_male_shirt.classList.remove("hide");
+
+    } else if((valueCountryOrigin == "bra" && valueCountryDestiny == "eur" && valueCategory == "male" && valueClothingType == "pants") || (valueCountryOrigin == "eur" && valueCountryDestiny == "bra" && valueCategory == "male" && valueClothingType == "pants")) {
+        result.classList.add("hide");
+        /* add for other tables: classlist.add.hide */
+        table_bra_eur_male_pants.classList.remove("hide");
+
+    } else if((valueCountryOrigin == "bra" && valueCountryDestiny == "eur" && valueCategory == "male" && valueClothingType == "shoe") || (valueCountryOrigin == "eur" && valueCountryDestiny == "bra" && valueCategory == "male" && valueClothingType == "shoe")) {
+        result.classList.add("hide");
+        /* add for other tables: classlist.add.hide */
+        table_bra_eur_male_shoe.classList.remove("hide");
+
+    /* choice: bra - eur - female */    
+    } else if((valueCountryOrigin == "bra" && valueCountryDestiny == "eur" && valueCategory == "female" && valueClothingType == "shirt") || (valueCountryOrigin == "eur" && valueCountryDestiny == "bra" && valueCategory == "female" && valueClothingType == "shirt")) {
+        result.classList.add("hide");
+        /* add for other tables: classlist.add.hide */
+        table_bra_eur_female_shirt.classList.remove("hide");
+
+    } else if((valueCountryOrigin == "bra" && valueCountryDestiny == "eur" && valueCategory == "female" && valueClothingType == "pants") || (valueCountryOrigin == "eur" && valueCountryDestiny == "bra" && valueCategory == "female" && valueClothingType == "pants")) {
+        result.classList.add("hide");
+        /* add for other tables: classlist.add.hide */
+        table_bra_eur_female_pants.classList.remove("hide");
+
+    } else if((valueCountryOrigin == "bra" && valueCountryDestiny == "eur" && valueCategory == "female" && valueClothingType == "shoe") || (valueCountryOrigin == "eur" && valueCountryDestiny == "bra" && valueCategory == "female" && valueClothingType == "shoe")) {
+        result.classList.add("hide");
+        /* add for other tables: classlist.add.hide */
+        table_bra_eur_female_shoe.classList.remove("hide");
+
+    /* choice: bra - eur - children */   
+    } else if((valueCountryOrigin == "bra" && valueCountryDestiny == "eur" && valueCategory == "children" && valueClothingType == "shirt") || (valueCountryOrigin == "eur" && valueCountryDestiny == "bra" && valueCategory == "children" && valueClothingType == "shirt")) {
+        result.classList.add("hide");
+        /* add for other tables: classlist.add.hide */
+        table_bra_eur_children_shirt.classList.remove("hide");
+
+    } else if((valueCountryOrigin == "bra" && valueCountryDestiny == "eur" && valueCategory == "children" && valueClothingType == "pants") || (valueCountryOrigin == "eur" && valueCountryDestiny == "bra" && valueCategory == "children" && valueClothingType == "pants")) {
+        result.classList.add("hide");
+        /* add for other tables: classlist.add.hide */
+        table_bra_eur_children_pants.classList.remove("hide");
+
+    } else if((valueCountryOrigin == "bra" && valueCountryDestiny == "eur" && valueCategory == "children" && valueClothingType == "shoe") || (valueCountryOrigin == "eur" && valueCountryDestiny == "bra" && valueCategory == "children" && valueClothingType == "shoe")) {
+        result.classList.add("hide");
+        /* add for other tables: classlist.add.hide */
+        table_bra_eur_children_shoe.classList.remove("hide");
+
     } else {
-    table.classList.add("hide");
-    result.textContent = "Atenção! Informação não encontrada."
+        table.classList.add("hide");
+        result.textContent = "Atenção! Informação não encontrada."
 }
 }
 
